e851386f1815b542c58819d1fd59a453e55668b4
Christian Leon Christensen <chrleon@gmail.com>
2023-11-13 21:58:27 +0100

    Accept parameters i Quick Actions

--- code.ts
+++ code.ts
@@ -9,6 +9,14 @@ let selection: SceneNode[] = []
 // for faster performance.
 figma.skipInvisibleInstanceChildren = true
 
+figma.parameters.on('input', ({ query, result }) => {
+  result.setSuggestions(
+    ['right','below','above','left']
+  .filter(s => s.includes(query)))
+})
+
+
+
 //  later we can filter the selection
 //  const texts = figma.currentPage.findAllWithCriteria({ types: ['TEXT'] })
 //  const boxes = figma.currentPage.findAllWithCriteria({ types: ['GROUP'] })
@@ -16,18 +24,32 @@ figma.skipInvisibleInstanceChildren = true
 //  filter layers with:
 // coordinates below and to the right of the user position
 // whose root node is the PAGE
-selection = figma.currentPage.children.filter((item, index) => {
-  // return item?.y > uy && item?.x > ux && item?.parent?.type == 'PAGE'
-  return item?.x > ux && item?.parent?.type == 'PAGE'
+figma.on('run', ({ command, parameters }: RunEvent) => {
+  selection = figma.currentPage.children.filter((item) => {
+    // return item?.y > uy && item?.x > ux && item?.parent?.type == 'PAGE'
+    switch (parameters?.direction) {
+      case 'right':
+        return item?.x > ux && item?.parent?.type == 'PAGE'
+      case 'left':
+        return item?.x < ux && item?.parent?.type == 'PAGE'
+      case 'above':
+        return item?.y < uy && item?.parent?.type == 'PAGE'
+      case 'below':
+        return item?.y > uy && item?.parent?.type == 'PAGE'
+      default:
+        return item?.x > ux && item?.parent?.type == 'PAGE'
+
+    }
+  })
+
+  const message = selection.length + " layers plucked. Do your thing!"
+  figma.notify(message);
+
+  // set the selection
+  figma.currentPage.selection = selection
+
+  //console.log(ux,uy,'run')
+  // Make sure to close the plugin when you're done. Otherwise the plugin will
+  // keep running, which shows the cancel button at the bottom of the screen.
+  figma.closePlugin();
 })
-
-const message = selection.length + " layers selected. Do your thing!"
-figma.notify(message);
-
-// set the selection
-figma.currentPage.selection = selection
-
-//console.log(ux,uy,'run')
-// Make sure to close the plugin when you're done. Otherwise the plugin will
-// keep running, which shows the cancel button at the bottom of the screen.
-figma.closePlugin();
